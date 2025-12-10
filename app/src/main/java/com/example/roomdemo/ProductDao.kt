5eac8ad13a7963465e3595672eb591024de5d5b7
v7ktory <v7ktory@gmail.com>
2025-12-10 22:06:04 +0700

    added Room Database

--- app/src/main/java/com/example/roomdemo/ProductDao.kt
+++ app/src/main/java/com/example/roomdemo/ProductDao.kt
@@ -1,9 +1,9 @@
+package com.example.roomdemo
+
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import com.example.roomdemo.Product
-
 @Dao
 interface ProductDao {
     @Insert
